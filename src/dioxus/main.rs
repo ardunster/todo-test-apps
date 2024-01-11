89a126d812b46c21466627689b73ea6097ba686a
Anna Dunster <anna@annardunster.com>
2024-01-10 21:00:18 -0800

    Hello world in Dioxus

--- src/dioxus/main.rs
+++ src/dioxus/main.rs
@@ -1,3 +1,15 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+
 fn main() {
     println!("Hello, Dioxus!");
+    dioxus_desktop::launch(App);
+}
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, Dioxus!"
+        }
+    })
 }
